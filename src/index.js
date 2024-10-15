25bc76523bb9555b7d636a2779dacff609839fd2
abdul061 <godofcoc346346@gmail.com~>
2024-10-15 10:16:22 +0530

    aa

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './Page/App';
-import About from './Page/About';
-import { MyProvider } from './Page/MyProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./Page/App";
+import About from "./Page/About";
+import { MyProvider } from "./Page/MyProvider";
 
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
@@ -21,4 +20,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
